e9ee14c106608c4b5fe909ac79072412e320e3f5
Joshua Mason <jtomason@hotmail.com>
2019-12-07 13:12:37 -0500

    Notifications API instead?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 const temp = 'https://young-earth-90471.herokuapp.com/get/five';
+
+Notification.requestPermission().then(function(result) {
+  console.log(result);
+});
+
+
+function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support system notifications");
+    // This is not how you would really do things if they aren't supported. :)
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+  // Finally, if the user has denied notifications and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
 function App() {
 
   const [info, setInfo] = useState(0);
@@ -10,6 +43,7 @@ function App() {
   //
   useEffect(() => {
     getInfo(setInfo)
+    
       console.log('mount it!');
   }, []); // passing an empty array as second argument triggers the callback in useEffect only after the initial render thus replicating `componentDidMount` lifecycle behaviour
 
@@ -22,6 +56,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={()=>{notifyMe()}}>Notify Me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -29,6 +64,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          
           {JSON.stringify(info)}
         </a>
       </header>
