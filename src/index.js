d7f84cd372236cf7d62d0823cd435b6cd282a898
Joshua Mason <jtomason@hotmail.com>
2019-12-07 07:35:47 -0500

    Trying one signal

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "f6a8f679-8059-4087-a1c8-5cbab81b5c99",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
