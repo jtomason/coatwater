abde91895de078c93bd868802a4161016fdeb9aa
Joshua Mason <jtomason@hotmail.com>
2019-12-15 22:11:42 -0500

    No capes

--- src/index.js
+++ src/index.js
@@ -6,24 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import history from "./utils/history";
 
-console.log('env', process.env.NODE_ENV);
-console.log(process.env);
-require('dotenv').config()
-console.log('env', process.env.NODE_ENV);
-console.log(process.env);
-
 let domain, client_id, audience;
 domain = process.env.REACT_APP_AUTH_DOMAIN;
 client_id = process.env.REACT_APP_AUTH_CLIENT_ID;
 audience = process.env.REACT_APP_AUDIENCE;
 
-if(window.location.href.includes("local") || process.env.REACT_APP_MODE==='local'){
-  let config = require("./auth_config.json");
-  domain = config.domain;
-  client_id = config.clientId;
-  audience = config.REACT_APP_AUDIENCE;
-  console.log("GOT CONFIG FILE", domain, client_id)
-}
 const onRedirectCallback = appState => {
   history.push(
     appState && appState.targetUrl
